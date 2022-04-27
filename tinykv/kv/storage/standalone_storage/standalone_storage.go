cec92fa85bab04f820e47705d2a6709f37642b03
loloxwg <loloxwg@gmail.com>
2022-04-28 01:57:16 +0800

    lab1P0 finished

--- tinykv/kv/storage/standalone_storage/standalone_storage.go
+++ tinykv/kv/storage/standalone_storage/standalone_storage.go
@@ -27,15 +27,33 @@ func (s *StandAloneStorage) Stop() error {
 
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
 	// YOUR CODE HERE (lab1).
-	panic("not implemented yet")
-	return nil, nil
+	txn := s.db.NewTransaction(false)
+	return NewBadgerReader(txn), nil
 }
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
 	// YOUR CODE HERE (lab1).
 	// Try to check the definition of `storage.Modify` and txn interface of `badger`.
 	// As the column family is not supported by `badger`, a wrapper is used to simulate it.
-	panic("not implemented yet")
+	txn := s.db.NewTransaction(true)
+	for _, m := range batch {
+		switch m.Data.(type) {
+		case storage.Put:
+			put := m.Data.(storage.Put)
+			if err := txn.Set(engine_util.KeyWithCF(put.Cf, put.Key), put.Value); err != nil {
+				return err
+			}
+		case storage.Delete:
+			del := m.Data.(storage.Delete)
+			if err := txn.Delete(engine_util.KeyWithCF(del.Cf, del.Key)); err != nil {
+				return err
+			}
+		}
+	}
+	if err := txn.Commit(); err != nil {
+		return err
+	}
+
 	return nil
 }
 
